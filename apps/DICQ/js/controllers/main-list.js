eaa0dad0e79152c6b55ef6d84fbefd293d1d4de0
Moky <albert.moky@gmail.com>
2020-03-24 15:06:38 +0800

    table view for main list

--- apps/DICQ/js/controllers/main-list.js
+++ apps/DICQ/js/controllers/main-list.js
@@ -2,43 +2,158 @@
 !function (ns, tui, dimp) {
     'use strict';
 
-    var $ = tui.$;
-
-    var Point = tui.Point;
-    var Size = tui.Size;
-    var Rect = tui.Rect;
-
-    var Label = tui.Label;
-    var Input = tui.Input;
-    var Button = tui.Button;
     var View = tui.View;
+    var Label = tui.Label;
+    var Button = tui.Button;
+    var Image = tui.Image;
+
+    var TableViewCell = tui.TableViewCell;
+    var TableViewDataSource = tui.TableViewDataSource;
+    var TableViewDelegate = tui.TableViewDelegate;
+    var TableView = tui.TableView;
 
     var Facebook = dimp.Facebook;
-    var Register = dimp.extensions.Register;
 
     var MainListView = function () {
-        View.call(this);
+        TableView.call(this);
 
-        var contacts = new Button();
-        contacts.setId('contactsBtn');
-        contacts.setClassName('contactsBtn buttonNormal');
-        contacts.setText('Contacts');
-        contacts.onClick = function () {
-            //
-        };
-        this.appendChild(contacts);
-
-        var groups = new Button();
-        groups.setId('groupsBtn');
-        groups.setClassName('groupsBtn buttonNormal');
-        groups.setText('Groups');
-        groups.onClick = function () {
-            //
-        };
-        this.appendChild(groups);
-
+        this.selectedIndex = 0;
+        this.dataSource = this;
+        this.delegate = this;
     };
-    dimp.Class(MainListView, View, null);
+    dimp.Class(MainListView, TableView, [TableViewDataSource, TableViewDelegate]);
+
+    MainListView.prototype.layoutSubviews = function () {
+        View.prototype.layoutSubviews.call(this);
+        this.reloadData();
+    };
+
+    //
+    //  TableViewDataSource/TableViewDelegate
+    //
+    MainListView.prototype.numberOfSections = function (tableView) {
+        return 2;
+    };
+
+    MainListView.prototype.viewForHeaderInSection = function (section, tableView) {
+        var button = new Button();
+        if (section === 0) {
+            button.setId('contactsBtn');
+            button.setClassName('contactsBtn buttonNormal');
+            button.setText('Contacts');
+            button.onClick = function () {
+                tableView.selectedIndex = 0;
+                tableView.reloadData();
+            };
+        } else {
+            button.setId('groupsBtn');
+            button.setClassName('groupsBtn buttonNormal');
+            button.setText('Groups');
+            button.onClick = function () {
+                tableView.selectedIndex = 1;
+                tableView.reloadData();
+            };
+        }
+        return button;
+    };
+
+    MainListView.prototype.numberOfRowsInSection = function (section, tableView) {
+        if (tableView.selectedIndex !== section) {
+            return 0;
+        }
+        if (section === 0) {
+            return get_contacts().length;
+        } else {
+            return get_groups().length;
+        }
+    };
+
+    var get_contacts = function () {
+        var facebook = Facebook.getInstance();
+        var user = facebook.getCurrentUser();
+        var contacts = facebook.getContacts(user.identifier);
+        if (!contacts) {
+            return [];
+        }
+        var list = [];
+        var id;
+        for (var i = 0; i < contacts.length; ++i) {
+            id = facebook.getInstance(contacts[i]);
+            if (id && id.isUser()) {
+                list.push(id);
+            }
+        }
+        return list;
+    };
+    var get_groups = function () {
+        var facebook = Facebook.getInstance();
+        var user = facebook.getCurrentUser();
+        var contacts = facebook.getContacts(user.identifier);
+        if (!contacts) {
+            return [];
+        }
+        var list = [];
+        var identifier;
+        for (var i = 0; i < contacts.length; ++i) {
+            identifier = facebook.getInstance(contacts[i]);
+            if (identifier && identifier.isGroup()) {
+                list.push(identifier);
+            }
+        }
+        return list;
+    };
+
+    MainListView.prototype.cellForRowAtIndexPath = function (indexPath, tableView) {
+        var cell = new TableViewCell();
+        var facebook = Facebook.getInstance();
+
+        var identifier;
+        var entity;
+        if (indexPath.section === 0) {
+            cell.setClassName('contactCell');
+            identifier = get_contacts()[indexPath.row];
+            entity = facebook.getUser(identifier);
+        } else {
+            cell.setClassName('groupCell');
+            identifier = get_groups()[indexPath.row];
+            entity = facebook.getGroup(identifier);
+        }
+        console.assert(identifier !== number, 'ID not found: ' + indexPath);
+        console.assert(entity !== number, 'ID error: ' + identifier);
+        var profile = entity.getProfile();
+
+        //
+        //  Avatar
+        //
+        var image;
+        if (identifier.isUser()) {
+            image = profile.getProperty('avatar');
+        } else {
+            // TODO: build group logo
+            image = null;
+        }
+        if (!image) {
+            image = 'https://dimchat.github.io/images/icon-512.png';
+        }
+        var img = new Image();
+        img.setClassName('avatar');
+        if (image) {
+            img.setSrc(image);
+        }
+        cell.appendChild(img);
+
+        //
+        //  Name(Number)
+        //
+        var name = entity.getName();
+        var number = facebook.getNumberString(identifier);
+        var label = new Label();
+        label.setClassName('name');
+        label.setText(name + ' (' + number + ')');
+        cell.appendChild(label);
+
+        return cell;
+    };
 
     ns.MainListView = MainListView;
 
